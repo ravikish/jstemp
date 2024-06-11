d7d1cf54e42b85f06db6d367988e0d61aa230f3f
ravikish <ravikishore9835@gmail.com>
2024-06-11 01:18:02 -0400

    DOM Manipulations

---
+++ TodoList/todo_list.js
@@ -0,0 +1,37 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+addTaskBtn.addEventListener("click", addTask);
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+let tasks = [];
+
+function addTask() {
+            const taskText = taskInput.value.trim();
+            if (taskText !== "") {
+                tasks.push({ text: taskText});
+                taskInput.value = "";
+                displayTasks();
+            }
+        }
+function displayTasks() {
+            taskList.innerHTML = "";
+            tasks.forEach((task, index) => {
+                const li = document.createElement("li");
+                li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+                    <label for="task-${index}">${task.text}</label>`;
+                li.querySelector("input").addEventListener("change", () => toggleTask(index));
+                taskList.appendChild(li);
+            });
+        }
+
+function toggleTask(index) {
+            tasks[index].completed = !tasks[index].completed;
+            displayTasks();
+        }
+
+function clearCompletedTasks() {
+            tasks = tasks.filter(task => !task.completed);
+            displayTasks();
+        }
